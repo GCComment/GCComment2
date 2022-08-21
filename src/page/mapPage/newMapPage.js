79ba5726d2a29d83a0749689fa771ffaeacc8b18
lukeIam <2lukeiam@gmail.com>
2022-08-21 18:23:11 +0200

    removed unused log message

--- src/page/mapPage/newMapPage.js
+++ src/page/mapPage/newMapPage.js
@@ -8,7 +8,6 @@ import { mapFinderHook } from "./mapHelper/newMapHook.js";
 export const gccommentOnNewMapPage = () => {
     waitForPropOfObject("L.Map", GMWindow, () => {
         mapFinderHook((mapInstance) => {
-            console.log(mapInstance._layers);
             setMap(mapInstance, onClickMarker);
             addGccMenu();
         });
