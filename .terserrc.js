515f30d8504b2ad9033276b6fc9de27a85f414be
lukeIam <2lukeiam@gmail.com>
2022-08-25 18:55:37 +0200

    renamed "output" to "format" to align with their docu

--- .terserrc.js
+++ .terserrc.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const header = fs.readFileSync("greasemonkey/greasemonkey.header", "utf8");
 
 module.exports = {
-    output: {
+    format: {
         preamble: `${header}`
     }
 };
