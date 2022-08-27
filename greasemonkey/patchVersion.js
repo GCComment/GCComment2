619f65682ca2a8f350d5b231fb08109aaa852608
lukeIam <2lukeiam@gmail.com>
2022-08-27 11:43:43 +0200

    Fix: having versiontags may breaks update mechanism

--- greasemonkey/patchVersion.js
+++ greasemonkey/patchVersion.js
@@ -3,16 +3,7 @@ var fs = require("fs");
 const rootUrl = "https://github.com/GCComment/GCComment2/releases/download";
 const tag = process.argv[2];
 
-// Rename map file
-fs.rename(
-    "dist/gccomment.user.js.map",
-    `dist/gccomment_${tag}.user.js.map`,
-    function (err) {
-        if (err) console.log("ERROR: " + err);
-    }
-);
-
-// rename script file and update link to map file
+// update link to map file and inject correct version into header
 fs.readFile("dist/gccomment.user.js", "utf8", function (err, data) {
     if (err) {
         return console.log(err);
@@ -29,17 +20,7 @@ fs.readFile("dist/gccomment.user.js", "utf8", function (err, data) {
         );
     }
 
-    fs.writeFile(
-        `dist/gccomment_${tag}.user.js`,
-        result,
-        "utf8",
-        function (err) {
-            if (err) return console.log(err);
-        }
-    );
-
-    // Delete old script file
-    fs.unlink("dist/gccomment.user.js", function (err) {
-        if (err) console.log("ERROR: " + err);
+    fs.writeFile(`dist/gccomment.user.js`, result, "utf8", function (err) {
+        if (err) return console.log(err);
     });
 });
